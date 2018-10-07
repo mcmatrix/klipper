369356873396a245140bf41f309e0ffa04d40993
Janar Sööt <janar.soot@gmail.com>
2018-10-07 13:47:21 +0300

    probe,klippy: add notify_event method
    Signed-off-by: Janar Sööt <janar.soot@gmail.com>

--- klippy/extras/probe.py
+++ klippy/extras/probe.py
@@ -170,6 +170,9 @@ class ProbePointsHelper:
         curpos[0] = x
         curpos[1] = y
         curpos[2] = self.horizontal_move_z
+        self.printer.notify_event(
+            "probe:next_position", self.toolhead.get_last_move_time(),
+            curpos, len(self.results), len(self.probe_points))
         try:
             self.toolhead.move(curpos, self.speed)
         except homing.EndstopError as e:
@@ -214,6 +217,8 @@ class ProbePointsHelper:
         self._move_next()
         if probe is None:
             # Setup for manual probing
+            self.printer.notify_event(
+                "probe:start_manual_probe", self.toolhead.get_last_move_time())
             self.gcode.register_command('NEXT', None)
             self.gcode.register_command('NEXT', self.cmd_NEXT,
                                         desc=self.cmd_NEXT_help)
@@ -225,14 +230,20 @@ class ProbePointsHelper:
     cmd_NEXT_help = "Move to the next XY position to probe"
     def cmd_NEXT(self, params):
         # Record current position for manual probe
+        self.printer.notify_event(
+            "probe:end_manual_probe", self.toolhead.get_last_move_time())
         self.toolhead.get_last_move_time()
         self.results.append(self.toolhead.get_kinematics().calc_position())
         # Move to next position
         self._move_next()
+        if self.busy:
+            self.printer.notify_event(
+                "probe:start_manual_probe", self.toolhead.get_last_move_time())
     def _finalize(self, success):
         self.busy = False
         self.gcode.reset_last_position()
         self.gcode.register_command('NEXT', None)
+        self.printer.notify_event("probe:finalize_probe", success)
         if success:
             self.finalize_callback(self.probe_offsets, self.results)
 
