79b32573041e00c8f32f3a7ff0beb8f9e1e76366
Janar Sööt <janar.soot@gmail.com>
2018-10-11 19:27:53 +0300

    probe_menu: small changes
    Signed-off-by: Janar Sööt <janar.soot@gmail.com>

--- klippy/extras/display/probe_menu.py
+++ klippy/extras/display/probe_menu.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2018  Janar Sööt <janar.soot@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging, os
+import os
 
 
 class ProbeHelperMenu:
@@ -12,27 +12,27 @@ class ProbeHelperMenu:
         self.printer = config.get_printer()
         self.toolhead = None
         # Load menu
+        self.probe_menuroot = "__probe_helper"
         self.menu = menu
         self.menu.load_config(os.path.dirname(__file__), 'probe_menu.cfg')
+        # check menuitem
+        self.menu.lookup_menuitem(self.probe_menuroot)
         # Probing context
         self._wait_for_input = False
         self._wizard_running = False
         self._points_current = 0
         self._points_count = 0
+        # Register event handler
+        self.printer.register_event_handler("probe:start_manual_probe",
+                                            self.handle_probe_start)
+        self.printer.register_event_handler("probe:end_manual_probe",
+                                            self.handle_probe_end)
+        self.printer.register_event_handler("probe:finalize_probe",
+                                            self.handle_probe_finalize)
 
     def printer_state(self, state):
         if state == 'ready':
             self.toolhead = self.printer.lookup_object('toolhead')
-
-    def printer_event(self, event, *args):
-        if event == "probe:start_manual_probe":
-            self.start_manual_probe(*args)
-        elif event == "probe:end_manual_probe":
-            self.end_manual_probe(*args)
-        elif event == "probe:next_position":
-            self.next_position(*args)
-        elif event == "probe:finalize_probe":
-            self.finalize_probe(*args)
 
     def get_status(self, eventtime):
         return {
@@ -45,43 +45,42 @@ class ProbeHelperMenu:
 
     def start_probe_wizard(self):
         if self.menu:
-            # self.menu.register_object(self, 'probe_menu', override=True)
+            self.menu.register_object(self, 'probe_menu', override=True)
             self._wizard_running = True
-            try:
-                self.menu.restart_root('__probe_helper')
-            except (self.menu.error, self.printer.config_error) as e:
-                msg = "Could not load probe menu.\n%s" % (e.message,)
-                logging.exception(msg)
-                self.gcode.respond_info(msg)
-                # self.menu.unregister_object('probe_menu')
-                self.menu.restart_root()
-                self.menu = None
-                self._wizard_running = False
+            self.menu.restart_root(self.probe_menuroot)
 
-    def close_probe_wizard(self):
+    def close_probe_wizard(self, eventtime):
         self._wizard_running = False
         if self.menu:
             self.menu.restart_root()
-            # self.menu.unregister_object('probe_menu')
+            self.menu.unregister_object('probe_menu')
 
-    # probe event methotds
-    def start_manual_probe(self, print_time):
+    def wait_toolhead_moves(self, eventtime, print_time):
+        est_print_time = self.toolhead.get_status(
+            eventtime)['estimated_print_time']
+        if est_print_time >= print_time:
+            self._wait_for_input = True
+        else:
+            self.menu.after(500, self.wait_toolhead_moves, print_time)
+
+    # probe event methods
+    def handle_probe_start(self, print_time):
         self._wait_for_input = False
         if not self._wizard_running:
             self.start_probe_wizard()
-        self.toolhead.wait_moves()
-        self._wait_for_input = True
+        reactor = self.printer.get_reactor()
+        self.wait_toolhead_moves(reactor.monotonic(), print_time)
 
-    def end_manual_probe(self, print_time):
+    def handle_probe_end(self, print_time):
         self._wait_for_input = False
 
     def next_position(self, print_time, curpos, index, length):
         self._points_current = index
         self._points_count = length
 
-    def finalize_probe(self, success):
+    def handle_probe_finalize(self, print_time, success):
         self._wait_for_input = False
-        self.close_probe_wizard()
+        self.menu.after(2000, self.close_probe_wizard)
 
 
 def load_config(config):
