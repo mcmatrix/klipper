52ea78e86f59395f4d27b3b9d8396dc25ecb72fe
Janar Sööt <janar.soot@gmail.com>
2019-05-25 18:55:56 +0300

    gcode_macro: simplified, removed expressions etc
    Signed-off-by: Janar Sööt <janar.soot@gmail.com>

--- klippy/extras/gcode_macro.py
+++ klippy/extras/gcode_macro.py
@@ -23,13 +23,8 @@ class StatusWrapper:
         self.eventtime = eventtime
         self.cache = {}
 
-    def __specialreplace(self, s):
-        """Replace triple underscores with spaces inside string.
-        First and last characters are excluded."""
-        return "".join((s[0], s[1:-1].replace('___', ' '), s[-1]))
-
     def __getitem__(self, val):
-        sval = self.__specialreplace(str(val).strip())
+        sval = str(val).strip()
         if sval in self.cache:
             return self.cache[sval]
         po = self.printer.lookup_object(sval, sentinel)
@@ -42,11 +37,11 @@ class StatusWrapper:
         return res
 
     def __setitem__(self, key, val):
-        skey = self.__specialreplace(str(key).strip())
+        skey = str(key).strip()
         self.cache[skey] = val
 
     def __contains__(self, val):
-        sval = self.__specialreplace(str(val).strip())
+        sval = str(val).strip()
         if sval not in self.cache and \
                 self.printer.lookup_object(sval, sentinel) is sentinel:
             return False
@@ -60,62 +55,14 @@ class StatusWrapper:
         return len(self.printer.lookup_objects())
 
 
-# Abstract wrapper around a Jinja2 environment
-class EnvironmentWrapper(object):
+# Wrapper around a Jinja2 template
+class TemplateWrapper():
     def __init__(self, printer, env, name, script):
         self.printer = printer
         self.name = name
         self.script = script
         self.env = env
         self.gcode = self.printer.lookup_object('gcode')
-
-    def create_status_wrapper(self, eventtime=None):
-        return StatusWrapper(self.printer, eventtime)
-
-    def create_default_context(self, context=None, eventtime=None):
-        if context is None:
-            context = {'status': self.create_status_wrapper(eventtime)}
-        defaults = {
-            'bool': bool
-        }
-        return dict(context, **defaults)
-
-    def _parse_ast(self):
-        return self.env.parse(self.script)
-
-    def find_calls(self):
-        """Finds all call names and constant arguments from the template."""
-        try:
-            ast = self._parse_ast()
-        except Exception as e:
-            msg = "Error parsing template '%s': %s" % (
-                self.name, traceback.format_exception_only(type(e), e)[-1])
-            logging.exception(msg)
-            raise self.gcode.error(msg)
-
-        for node in ast.find_all(jinja2.nodes.Call):
-            if not isinstance(node.node, jinja2.nodes.Name):
-                continue
-            args = []
-            for arg in node.args:
-                if isinstance(arg, jinja2.nodes.Const):
-                    args.append(arg.value)
-                else:
-                    args.append(None)
-            for arg in node.kwargs:
-                args.append(None)
-            if node.dyn_args is not None:
-                args.append(None)
-            if node.dyn_kwargs is not None:
-                args.append(None)
-            args = tuple(x for x in args if x is not None)
-            yield node.node.name, args
-
-
-# Wrapper around a Jinja2 template
-class TemplateWrapper(EnvironmentWrapper):
-    def __init__(self, printer, env, name, script):
-        super(TemplateWrapper, self).__init__(printer, env, name, script)
         try:
             self.template = env.from_string(script)
         except Exception as e:
@@ -124,8 +71,12 @@ class TemplateWrapper(EnvironmentWrapper):
             logging.exception(msg)
             raise printer.config_error(msg)
 
+    def create_status_wrapper(self, eventtime=None):
+        return StatusWrapper(self.printer, eventtime)
+
     def render(self, context=None):
-        context = self.create_default_context(context)
+        if context is None:
+            context = {'status': self.create_status_wrapper()}
         try:
             return str(self.template.render(context))
         except Exception as e:
@@ -136,36 +87,6 @@ class TemplateWrapper(EnvironmentWrapper):
 
     def run_gcode_from_command(self, context=None):
         self.gcode.run_script_from_command(self.render(context))
-
-
-# Wrapper around a Jinja2 expression
-class ExpressionWrapper(EnvironmentWrapper):
-    def __init__(self, printer, env, name, script):
-        super(ExpressionWrapper, self).__init__(printer, env, name, script)
-        try:
-            self.expression = env.compile_expression(script)
-        except Exception as e:
-            msg = "Error loading expression '%s': %s" % (
-                name, traceback.format_exception_only(type(e), e)[-1])
-            logging.exception(msg)
-            raise printer.config_error(msg)
-
-    def _parse_ast(self):
-        return self.env.parse("".join([
-            self.env.variable_start_string,
-            self.script,
-            self.env.variable_end_string
-        ]))
-
-    def evaluate(self, context=None):
-        context = self.create_default_context(context)
-        try:
-            return self.expression(context)
-        except Exception as e:
-            msg = "Error evaluating '%s': %s" % (
-                self.name, traceback.format_exception_only(type(e), e)[-1])
-            logging.exception(msg)
-            raise self.gcode.error(msg)
 
 
 # Main gcode macro template tracking
@@ -183,13 +104,6 @@ class PrinterGCodeMacro:
         script = (config.get(option) if default is sentinel
                   else config.get(option, default))
         return TemplateWrapper(self.printer, self.env, name, script)
-
-    def load_expression(self, config, option, default=sentinel):
-        name = "%s:%s" % ('<dict>' if isinstance(config, dict)
-                          else config.get_name(), option)
-        script = (config.get(option) if default is sentinel
-                  else config.get(option, default))
-        return ExpressionWrapper(self.printer, self.env, name, script)
 
     def create_status_wrapper(self, eventtime=None):
         return StatusWrapper(self.printer, eventtime)
