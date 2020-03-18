3632143b35b5e67fe7e0f8edec2bf86ce28c9679
Janar Sööt <janar.soot@gmail.com>
2020-03-18 13:09:06 +0200

    menu: draw fix
    Signed-off-by: Janar Sööt <janar.soot@gmail.com>

--- klippy/extras/display/menu.py
+++ klippy/extras/display/menu.py
@@ -952,7 +952,7 @@ class MenuManager:
         if self.is_running():
             self.lcd_chip.clear()
             for y, line in enumerate(self.render(eventtime)):
-                self.display.draw_text(0, y, line, eventtime)
+                self.display.draw_text(y, 0, line, eventtime)
             self.lcd_chip.flush()
             return eventtime + MENU_UPDATE_DELAY
         else:
