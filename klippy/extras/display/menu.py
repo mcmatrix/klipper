f9b5d587dddfda29d41012bcdfa89b696374d53d
Janar Sööt <janar.soot@gmail.com>
2020-03-20 10:54:37 +0200

    menu: small sdcard lookup fix
    Signed-off-by: Janar Sööt <janar.soot@gmail.com>

--- klippy/extras/display/menu.py
+++ klippy/extras/display/menu.py
@@ -592,7 +592,7 @@ class MenuVSDList(MenuList):
 
     def _populate(self):
         super(MenuVSDList, self)._populate()
-        sdcard = self.manager.printer.lookup_object('virtual_sdcard')
+        sdcard = self.manager.printer.lookup_object('virtual_sdcard', None)
         if sdcard is not None:
             files = sdcard.get_file_list()
             for fname, fsize in files:
